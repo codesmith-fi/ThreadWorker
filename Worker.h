3316c7ac52a1e3c89313ed1a28518e49e21b2d3d
Erno Pakarinen <codesmith.fi@gmail.com>
2021-06-03 23:23:51 +0300

    Refactored Worker to single header

--- Worker.h
+++ Worker.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <mutex>
 #include <functional>
+#include "DebugLogger.h"
 
 class Worker
 {
@@ -48,23 +49,66 @@ public: // enums
 	static const uint16_t KThreadWaitSeconds = 1;
 
 public:
-	Worker();
-	Worker(uint16_t id);
-	virtual ~Worker();
+	Worker() : Worker(0) { };
+	Worker(uint16_t id) : m_thread{}, m_state(WorkerState::ECreated), 
+	m_id(id), m_running(false), m_usefunction(false) {
+		LOG_INFO() << "Created Worker number: " << m_id;
+	};
+	virtual ~Worker() { 
+		LOG_INFO() << "Destroyed Worker number: " << m_id;
+	};
 
 public:
 	std::uint16_t id() const { return m_id; };
 	const WorkerState& state() const { return m_state; }
-	void start();
-	void stop();
-	void work();
-	void work(std::function<void(void)>);
+	void start() {
+		LOG_INFO() << "Starting worker: " << m_id;
+		m_running = true;
+		m_state = WorkerState::EIdle;
+		m_thread = std::thread([this]() { doRun(); });
+	};
+	void stop() {
+		LOG_INFO() << "Stopping worker: " << m_id;
+		if (m_running) {
+			m_running = false;
+			m_state = WorkerState::EExiting;
+			m_thread.join();
+		}
+	};
+	void work() {
+		const std::lock_guard<std::mutex> lock(m_mutex);
+		if (m_state == WorkerState::EIdle) {
+			m_usefunction = false;
+			m_state = WorkerState::EWorking;
+		}
+	};
+	void work(std::function<void(void)> task) {
+		const std::lock_guard<std::mutex> lock(m_mutex);
+		if (m_state == WorkerState::EIdle) {
+			m_task = task;
+			m_usefunction = true;
+			m_state = WorkerState::EWorking;
+		}
+	}
 
 protected:
-	virtual void doWork();
+	virtual void doWork() {
+		LOG_INFO() << "Default worker: " << m_id << " work ended\n";	
+	}
 
 private:
-	void doRun();
+	void doRun() {
+		while(m_running) {	
+			if (m_state == WorkerState::EWorking) {
+				m_task();
+				m_state = WorkerState::EIdle;
+			}
+			else {
+				LOG_INFO() << "Worker: " << m_id << " @" << Worker::WorkerStateToString(m_state);
+				std::this_thread::sleep_for(std::chrono::seconds(KThreadWaitSeconds));
+			}
+		}
+	}
 
 private:
 	std::thread m_thread;
